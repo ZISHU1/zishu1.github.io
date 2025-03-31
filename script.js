095173f54674c4a3e5ba7560553478b157aca19a
UmIamBda Doe <87239056+ZISHU1@users.noreply.github.com>
2025-03-31 15:37:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function updateCountdown() {
-  const endDate = new Date('March 31, 2025 00:00:00').getTime();
+  const endDate = new Date('April 1, 2025 00:00:00').getTime();
   const now = new Date().getTime();
   const timeLeft = endDate - now;
 
